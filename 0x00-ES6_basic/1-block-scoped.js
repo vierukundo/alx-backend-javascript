c68d117937f6cf07485315f46470dd94d7b73050
vierukundo <vierukundo@gmail.com>
2024-01-04 16:42:40 +0000

    Disabled eslint for some lines

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,8 +3,9 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+	  // eslint-disable-line
+    const task = true; // eslint-disable-line
+    const task2 = false; // eslint-disable-line
   }
 
   return [task, task2];
