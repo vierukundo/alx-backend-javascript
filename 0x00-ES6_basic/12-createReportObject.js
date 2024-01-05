e323ef8d49cec8e34af2303d0968427cfeeea18f
vierukundo <vierukundo@gmail.com>
2024-01-05 21:24:53 +0000

    Applying ES6 method property syntax

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,10 @@
+export default function createReportObject(employeesList) {
+  const obj = {
+    allEmployees: { ...employeesList },
+    getNumberOfDepartments(employeesList) {
+      return Object.keys(employeesList).length;
+    },
+  };
+
+  return obj;
+}
