cb53998601077ac6ec613c56d2c4baa159d13520
vierukundo <vierukundo@gmail.com>
2024-01-06 23:59:48 +0000

    Handle multiple promises

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,13 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  const promise1 = signUpUser(firstName, lastName);
+  const promise2 = uploadPhoto(fileName);
+
+  return Promise.allSettled([promise1, promise2])
+    .then((results) => results.map((result, index) => ({
+      status: result.status,
+      value: index === 0 ? result.value : result.reason,
+    })));
+}
