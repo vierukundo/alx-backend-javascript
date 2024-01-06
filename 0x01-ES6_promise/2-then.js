f417b93603b4dbceb7fa591937c81d54f71641a6
vierukundo <vierukundo@gmail.com>
2024-01-06 21:33:02 +0000

    Used then, catch and finally promise methods

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,11 +1,9 @@
 export default function handleResponseFromAPI(promise) {
   promise
-    .then(() => {
-      return { 
-        status: 200,
-	body: success,
-      }
-    })
+    .then(() => ({
+      status: 200,
+      body: 'success',
+    }))
     .catch(() => new Error())
     .finally(() => {
       console.log('Got a response from the API');
