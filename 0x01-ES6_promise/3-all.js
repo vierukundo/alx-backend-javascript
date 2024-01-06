38d730cf5ba97c11e83b9f5a084764ecd768ad21
vierukundo <vierukundo@gmail.com>
2024-01-06 22:45:31 +0000

    handling multiple promises

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,15 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const promise1 = uploadPhoto();
+  const promise2 = createUser();
+
+  Promise.all([promise1, promise2])
+    .then((results) => {
+      const [uploadPhotoResult, createUserResult] = results;
+      console.log(`${uploadPhotoResult.body} ${createUserResult.firstName} ${createUserResult.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
