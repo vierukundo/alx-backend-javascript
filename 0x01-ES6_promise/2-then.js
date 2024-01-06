9f4712f4b0144d991abaf61cf81d98bd026e7ff8
vierukundo <vierukundo@gmail.com>
2024-01-06 20:31:03 +0000

    Used then method

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,8 +3,8 @@ export default function handleResponseFromAPI(promise) {
     .then(() => {
       console.log('Got a response from the API');
       return { status: 200, body: 'success' };
-    })
-    .catch(() => {
+    },
+    () => {
       console.log('Got a response from the API');
       return new Error();
     });
