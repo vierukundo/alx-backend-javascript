2b2a4c74c3db06909a4d174423ae5a5e61313eec
vierukundo <vierukundo@gmail.com>
2024-01-07 01:24:02 +0000

    removed eslint-disable line

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,4 +1,4 @@
-import getBudgetObject from './7-getBudgetObject.js'; // eslint-disable-line
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
