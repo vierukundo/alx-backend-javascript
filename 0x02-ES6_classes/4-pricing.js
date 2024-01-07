3518f579a0a8347e90262414b2eeb6e1bc4c5334
vierukundo <vierukundo@gmail.com>
2024-01-07 21:27:52 +0000

    Pricing class

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,23 @@
+// eslint-disable-next-line no-unused-vars
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this._currency._name} (${this._currency._code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof conversionRate !== 'number' || typeof amount !== 'number') {
+      throw new TypeError('conversionRate or Amount must be a number');
+    }
+    return amount * conversionRate;
+  }
+}
