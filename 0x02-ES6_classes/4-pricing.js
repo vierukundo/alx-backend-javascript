a94885d10322c69701f7becd90cc9d2f9908ec5e
vierukundo <vierukundo@gmail.com>
2024-01-07 21:50:06 +0000

    Implemented pricing class

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -12,6 +12,28 @@ export default class Pricing {
     this._currency = currency;
   }
 
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = value;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(value) {
+    if (!(value instanceof Currency)) {
+      throw new TypeError('currency must be a Currency object');
+    }
+    this._currency = value;
+  }
+
   displayFullPrice() {
     return `${this._amount} ${this._currency._name} (${this._currency._code})`;
   }
