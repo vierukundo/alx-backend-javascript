8107ee41f15a16ea441295c02b144206018a5424
vierukundo <vierukundo@gmail.com>
2024-01-09 23:03:16 +0000

    clean set with slice method

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,8 @@
 export default function cleanSet(inputSet, startString) {
-  if (!(inputSet instanceof Set) || typeof startString !== 'string') {
-    throw new Error('Invalid input types. Expecting a Set and a string.');
-  }
-  if (startString === '') {
+  if (!(inputSet instanceof Set) || typeof startString !== 'string' || !startString || !inputSet) {
     return '';
   }
+
   const result = Array.from(inputSet)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
