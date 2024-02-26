1d36013032c1f710f6ca977d4ce82ebbd0427c70
vierukundo <vierukundo@gmail.com>
2024-02-26 23:05:12 +0000

    updated code to pass eslint

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,15 +1,15 @@
 // 1-stdin.js
 
-console.log('Welcome to Holberton School, what is your name?')
+console.log('Welcome to Holberton School, what is your name?');
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('readable', function() {
-  var chunk = process.stdin.read();
+process.stdin.on('readable', () => {
+  const chunk = process.stdin.read();
   if (chunk !== null) {
-    process.stdout.write('Your name is: ' + chunk);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
-process.stdin.on('end', function() {
+process.stdin.on('end', () => {
   process.stdout.write('This important software is now closing\n');
 });
