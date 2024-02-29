f2af277ae7115c6b312f5b09945b3514c872c16c
vierukundo <vierukundo@gmail.com>
2024-02-29 21:42:51 +0000

    first nodejs unit test

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,11 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul.js');
+
+describe('calculateNumber', () => {
+  it('should round two numbers and return their sum', () => {
+    assert.strictEqual(calculateNumber(1, 3), 4);
+    assert.strictEqual(calculateNumber(1, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.2, 3.7), 5);
+    assert.strictEqual(calculateNumber(1.5, 3.7), 6);
+  });
+});
