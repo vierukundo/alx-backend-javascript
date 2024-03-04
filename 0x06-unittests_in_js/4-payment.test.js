5d770d35558422ab718ee7ae9ad4ef6defa45354
vierukundo <vierukundo@gmail.com>
2024-03-04 12:34:34 +0000

    test using sinon.stub

---
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -0,0 +1,28 @@
+// 4-payment.test.js
+const { expect } = require('chai');
+const sinon = require('sinon');
+const sendPaymentRequestToApi = require('./4-payment');  // Update to 4-payment.js
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+  it('should call Utils.calculateNumber with correct arguments and log the sum', () => {
+    // Stub Utils.calculateNumber to always return 10
+    const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+
+    // Spy on console.log
+    const consoleSpy = sinon.spy(console, 'log');
+
+    // Call the function
+    sendPaymentRequestToApi(100, 20);
+
+    // Assert that Utils.calculateNumber was called with the correct arguments
+    sinon.assert.calledWithExactly(calculateNumberStub, 'SUM', 100, 20);
+
+    // Assert that console.log was called with the correct message
+    sinon.assert.calledWithExactly(consoleSpy, 'The total is: 10');
+
+    // Restore the stub and spy after the test
+    calculateNumberStub.restore();
+    consoleSpy.restore();
+  });
+});
