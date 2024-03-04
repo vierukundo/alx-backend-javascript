46ded6530ab5be7303e389d3dc45f1c0445ebaab
vierukundo <vierukundo@gmail.com>
2024-03-04 15:10:36 +0000

    test new endpoint for number parameter

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,12 +1,27 @@
-const { expect } = require('chai')
-const request = require('request')
+const { expect } = require('chai');
+const request = require('request');
 
-describe('server test', () => {
-    it('tests the server', (done) => {
-        request.get('http://localhost:7865', (err, res, body) => {
-            expect(res.statusCode).to.equal(200)
-            expect(body).to.equal('Welcome to the payment system')
-            done()
-        })
-    })
-})
+describe('Server Test', () => {
+  it('tests the server', (done) => {
+    request.get('http://localhost:7865', (err, res, body) => {
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Welcome to the payment system');
+      done();
+    });
+  });
+
+  it('should respond with a 200 status code and the correct message for the cart page when :id is a number', (done) => {
+    request.get('http://localhost:7865/cart/123', (error, response, body) => {
+      expect(response.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart 123');
+      done();
+    });
+  });
+
+  it('should respond with a 404 status code when accessing the cart page with a non-number :id', (done) => {
+    request.get('http://localhost:7865/cart/abc', (error, response, body) => {
+      expect(response.statusCode).to.equal(404);
+      done();
+    });
+  });
+});
