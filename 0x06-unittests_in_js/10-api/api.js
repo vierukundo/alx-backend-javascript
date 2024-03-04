8c3452fe636dbbfd48ca1d0d17f52ebb67086d30
vierukundo <vierukundo@gmail.com>
2024-03-04 17:03:24 +0000

    test for login endpoint

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,38 +1,28 @@
-const express = require('express');
+const express = require('express')
+const app = express()
+app.use(express.json());
 
-// Create an instance of express
-const app = express();
-const port = 7865;
-
-// Define a route for GET /
 app.get('/', (req, res) => {
-  res.send('Welcome to the payment system');
-});
-
-// New endpoint for cart page
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  const cartId = req.params.id;
-  res.send(`Payment methods for cart ${cartId}`);
-});
-
-// New endpoint for /available_payments
-app.get('/available_payments', (req, res) => {
-  res.json({
-    payment_methods: {
-      credit_cards: true,
-      paypal: false
-    }
+    res.send('Welcome to the payment system');
   });
-});
 
-// New endpoint for POST /login
+app.get('/cart/:id([0-9]+)', (req, res) => {
+    res.send(`Payment methods for cart ${req.params.id}`);
+  });
+
+app.get('/available_payments', (req, res) => {
+    res.json({
+      payment_methods: {
+        credit_cards: true,
+        paypal: false
+      }
+    })
+})
+
 app.post('/login', (req, res) => {
-  const userName = req.body.userName;
-  res.send(`Welcome ${userName}`);
-});
+  const username = req.body.userName
+  res.send(`Welcome ${username}`)
+})
 
-// Start the express server and listen to port 7865
-app.listen(port, () => {
-  console.log(`API available on http://localhost:${port}`);
-});
-
+app.listen(7865, () => {console.log('API available on localhost port 7865')});
+module.exports = app;
